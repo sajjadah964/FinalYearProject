b3788f03c4121b9b88cb32447ced9911072726b7
umairkhanlel <umairkhanlel9978@gmail.com>
2023-05-02 17:48:48 +0500

    checkkrin yar sird nava folder banaya ha resetpassword os kay andar ResetPassword.js

--- EatMore/front-end/src/Navigation/Routes.js
+++ EatMore/front-end/src/Navigation/Routes.js
@@ -12,7 +12,7 @@ function Routes() {
         <NavigationContainer>
             <Stack.Navigator
                 screenOptions={{ presentation: 'card', headerShown: false }}
-                initialRouteName={NavigationStrings.HOME}
+                initialRouteName={NavigationStrings.SIGNUP}
             >
                 {/* {user ? MainStack(Stack) : AuthStack(Stack)} */}
                 {MainStack(Stack)}
