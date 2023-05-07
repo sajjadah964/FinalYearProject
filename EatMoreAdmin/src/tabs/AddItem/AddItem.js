92166885572896dfa033263c9a33d8d60b928a70
Sajjad Ahmad <73050766+sajjadah964@users.noreply.github.com>
2023-05-07 23:43:53 +0500

    item add in the firebase

--- EatMoreAdmin/src/tabs/AddItem/AddItem.js
+++ EatMoreAdmin/src/tabs/AddItem/AddItem.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, ScrollView, PermissionsAndroid, ToastAndroid, Alert } from 'react-native'
 import CustomHeader from '../../components/CustomHeader'
 import { moderateScale, moderateVerticalScale, scale } from 'react-native-size-matters'
 import Colors from '../../styles/Colors';
@@ -9,7 +9,9 @@ import CustomPkgBtn from '../../components/CustomPkgBtn';
 import CustomModal from '../../constants/CustomModal';
 import imagePath from '../../constants/imagePath';
 import Loader from '../../components/Loader';
-
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
 const AddItem = () => {
     const [isLoading, setisLoading] = useState(false);
 
@@ -17,7 +19,23 @@ const AddItem = () => {
     const [price, setPrice] = useState('');
     const [points, setPoints] = useState('');
     const [description, setDescription] = useState('');
-    const [selectedIndex, setSelectedIndex] = useState(0)
+    const [category, setCategory] = useState('');
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [imageData, setImageData] = useState(null);
+    const buttons = [
+        {
+            id: 1,
+            title: 'All Items'
+        },
+        {
+            id: 2,
+            title: 'Burger'
+        },
+        {
+            id: 3,
+            title: 'Pizza'
+        }
+    ]
     const selectCategory = (index) => {
         setSelectedIndex(index)
     }
@@ -28,6 +46,90 @@ const AddItem = () => {
             return styles.unselectedButton;
         }
     };
+    const requestCameraPermission = async () => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.CAMERA,
+                {
+                    title: 'Cool Photo App Camera Permission',
+                    message:
+                        'Cool Photo App needs access to your camera ' +
+                        'so you can take awesome pictures.',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('You can use the camera');
+                openGallery();
+            } else {
+                console.log('Camera permission denied');
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+    };
+    const openGallery = async () => {
+        const result = await launchImageLibrary({ mediaType: 'photo' });
+        if (result.didCancel) {
+        } else {
+            console.log(result);
+            setImageData(result);
+        }
+    };
+
+    const uploadItem = async (url) => {
+        if (!name || !price || !points || !description || !imageData) {
+          ToastAndroid.show('Please enter all data', ToastAndroid.SHORT);
+          return;
+        }
+      
+        let imageUploaded = false;
+      
+        if (imageData.assets[0] !== '') {
+          try {
+            const reference = storage().ref(imageData.assets[0].fileName);
+            const pathToFile = imageData.assets[0].uri;
+            // uploads file
+            await reference.putFile(pathToFile);
+            url = await storage()
+              .ref(imageData.assets[0].fileName)
+              .getDownloadURL();
+            console.log(url);
+            imageUploaded = true;
+          } catch (error) {
+            ToastAndroid.show('Image upload failed', ToastAndroid.SHORT);
+            console.log(error);
+          }
+        }
+        firestore()
+          .collection('items')
+          .add({
+            name: name,
+            price: price,
+            points: points,
+            description: description,
+            category:
+              selectedIndex == 0 ? 'All Items' : selectedIndex == 1 ? 'Burger' : 'Pizza',
+            imageUrl: imageUploaded ? url : null,
+          })
+          .then(() => {
+            ToastAndroid.show('Item Added', ToastAndroid.SHORT);
+          })
+          .catch((error) => {
+            ToastAndroid.show('Item added failed', ToastAndroid.SHORT);
+            console.log(error);
+          });
+      
+        setName('');
+        setPrice('');
+        setPoints('');
+        setDescription('');
+        setSelectedIndex(0);
+        setImageData('');
+      };
+      
     // LOADING CODE
     // useEffect(() => {
     //     setTimeout(() => {
@@ -45,104 +147,120 @@ const AddItem = () => {
                     // headerImgStyle={styles.headerImgStyle}
                     />
                     <ScrollView style={{ flex: 1 }}
-                      showsVerticalScrollIndicator={false}
-                      showsHorizontalScrollIndicator={false}
+                        showsVerticalScrollIndicator={false}
+                        showsHorizontalScrollIndicator={false}
                     >
-                    <View style={styles.FormInputView}>
+                        <View style={styles.FormInputView}>
 
-                        <TextInputWithLabel
-                            // label={'Department'}
-                            labelTextStyle={styles.labelTextStyle}
-                            inputStyle={styles.inputStyle}
-                            placeHolder="Name"
-                            inlineInputStyle={styles.inlineInputStyle}
-                            placeholderTextColor='rgba(0, 0, 0, 0.3)'
-                            value={name}
-                            onChangeText={setName}
-                        />
-                        <TextInputWithLabel
-                            // label={'Gender'}
-                            labelTextStyle={styles.labelTextStyle}
-                            inputStyle={styles.inputStyle}
-                            placeHolder="Item Price"
-                            inlineInputStyle={styles.inlineInputStyle}
-                            placeholderTextColor='rgba(0, 0, 0, 0.3)'
-                            value={price}
-                            onChangeText={setPrice}
-                        />
-                        <TextInputWithLabel
-                            // label={'City'}
-                            labelTextStyle={styles.labelTextStyle}
-                            inputStyle={styles.inputStyle}
-                            placeHolder="Item Points"
-                            inlineInputStyle={styles.inlineInputStyle}
-                            placeholderTextColor='rgba(0, 0, 0, 0.3)'
-                            value={points}
-                            onChangeText={setPoints}
-                        />
-                        <TextInputWithLabel
-                            // label={'Address'}
-                            labelTextStyle={styles.labelTextStyle}
-                            inputStyle={{ ...styles.inputStyle, ...styles.descInputStyle }}
-                            placeHolder="Enter Your Description"
-                            inlineInputStyle={styles.inlineInputStyle}
-                            placeholderTextColor='rgba(0, 0, 0, 0.3)'
-                            value={description}
-                            onChangeText={setDescription}
-                        />
-                        <View style={styles.categoryView}>
-                            <Text style={styles.categorylabel}>Select Your Category</Text>
-                            <View style={styles.categoryBtnView}>
-                                < CustomPkgBtn
-                                    btnText={'All Items'}
-                                    textStyle={{ ...styles.textStyle, ...styles.categoryTextStyle, color: selectedIndex == 0 ? '#FFF' : '#A8A7A7' }}
-                                    btnStyle={{ ...styles.btnStyle, ...getButtonStyle(0) }}
-                                    onPress={() => selectCategory(0)}
-                                />
+                            <TextInputWithLabel
+                                // label={'Department'}
+                                labelTextStyle={styles.labelTextStyle}
+                                inputStyle={styles.inputStyle}
+                                placeHolder="Item Name"
+                                inlineInputStyle={styles.inlineInputStyle}
+                                placeholderTextColor='rgba(0, 0, 0, 0.3)'
+                                value={name}
+                                onChangeText={setName}
+                            />
+                            <TextInputWithLabel
+                                // label={'Gender'}
+                                labelTextStyle={styles.labelTextStyle}
+                                inputStyle={styles.inputStyle}
+                                placeHolder="Item Price"
+                                inlineInputStyle={styles.inlineInputStyle}
+                                placeholderTextColor='rgba(0, 0, 0, 0.3)'
+                                value={price}
+                                onChangeText={setPrice}
+                            />
+                            <TextInputWithLabel
+                                // label={'City'}
+                                labelTextStyle={styles.labelTextStyle}
+                                inputStyle={styles.inputStyle}
+                                placeHolder="Item Points"
+                                inlineInputStyle={styles.inlineInputStyle}
+                                placeholderTextColor='rgba(0, 0, 0, 0.3)'
+                                value={points}
+                                onChangeText={setPoints}
+                            />
+                            <TextInputWithLabel
+                                // label={'Address'}
+                                labelTextStyle={styles.labelTextStyle}
+                                inputStyle={{ ...styles.inputStyle, ...styles.descInputStyle }}
+                                placeHolder="Enter Your Description"
+                                inlineInputStyle={styles.inlineInputStyle}
+                                placeholderTextColor='rgba(0, 0, 0, 0.3)'
+                                value={description}
+                                onChangeText={setDescription}
+                            />
+                            <View style={styles.categoryView}>
+                                <Text style={styles.categorylabel}>Select Your Category</Text>
+                                <View style={styles.categoryBtnView}>
+                                    {buttons.map((button, index) => {
+                                        return (
+                                            <CustomPkgBtn
+                                                key={index}
+                                                btnText={button.title}
+                                                textStyle={{ ...styles.textStyle, ...styles.categoryTextStyle, color: selectedIndex == index ? '#FFF' : '#A8A7A7' }}
+                                                btnStyle={{ ...styles.btnStyle, ...getButtonStyle(index) }}
+                                                onPress={() => selectCategory(index)}
+                                            />
+                                        )
+                                    })
 
-
-                                <CustomPkgBtn
-                                    btnText={'Burger'}
-                                    textStyle={{ ...styles.textStyle, ...styles.categoryTextStyle, color: selectedIndex == 1 ? '#FFF' : '#A8A7A7' }}
-                                    btnStyle={{ ...styles.btnStyle, ...getButtonStyle(1) }}
-                                    onPress={() => selectCategory(1)}
-                                />
-
-
-                                <CustomPkgBtn
-                                    btnText={'pIzza'}
-                                    textStyle={{ ...styles.textStyle, ...styles.categoryTextStyle, color: selectedIndex == 2 ? '#FFF' : '#A8A7A7' }}
-                                    btnStyle={{ ...styles.btnStyle, ...getButtonStyle(2) }}
-                                    onPress={() => selectCategory(2)}
-                                />
-
+                                    }
+                                </View>
                             </View>
-                        </View>
-                        <View style={styles.addImageView}>
-                            <Text style={styles.categorylabel}>Add Your Image</Text>
-                            <View style={{
-                                alignItems: 'center',
-                                justifyContent: 'center'
-                            }}>
-                                <TouchableOpacity style={styles.addImageStyle}
-                                    activeOpacity={0.7}
-                                >
-                                    <Image
-                                        style={{
-                                            justifyContent: 'center',
-                                            alignItems: 'center',
+                            <View style={styles.addImageView}>
+                                <Text style={styles.categorylabel}>Add Your Image</Text>
+                                <View style={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}>
+                                    <TouchableOpacity style={styles.addImageStyle}
+                                        activeOpacity={0.7}
+                                        onPress={() => {
+                                            requestCameraPermission();
                                         }}
-                                        source={imagePath.icAddImage}
-                                    />
-                                </TouchableOpacity>
+                                    >
+                                        {/* <Image
+                                            style={{
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                            }}
+                                            source={imagePath.icAddImage}
+                                        /> */}
+                                        {imageData !== null ? (
+                                            <Image
+                                                source={{ uri: imageData.assets[0].uri }}
+                                                style={{
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center',
+                                                    width: '100%',
+                                                    height: '100%',
+                                                    borderRadius: 10
+                                                }}
+                                            />
+                                        ) :
+                                            <Image
+                                                style={{
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center',
+                                                }}
+                                                source={imagePath.icAddImage}
+                                            />
+                                        }
+                                    </TouchableOpacity>
+                                </View>
                             </View>
+                            <CustomPkgBtn
+                                btnText={'Upload Item'}
+                                textStyle={{ ...styles.textStyle }}
+                                btnStyle={{ ...styles.btnStyle }}
+                                onPress={() => {
+                                    uploadItem();
+                                }}
+                            />
                         </View>
-                        <CustomPkgBtn
-                            btnText={'Upload Item'}
-                            textStyle={{ ...styles.textStyle }}
-                            btnStyle={{ ...styles.btnStyle }}
-                        />
-                    </View>
                     </ScrollView>
                 </View>
             }
@@ -179,11 +297,11 @@ const styles = StyleSheet.create({
         paddingHorizontal: moderateScale(15)
     },
     btnStyle: {
-        width:'95%',
+        width: '92%',
         height: moderateScale(48),
         backgroundColor: '#50379E',
         marginTop: moderateVerticalScale(22),
-        marginBottom:moderateVerticalScale(90)
+        marginBottom: moderateVerticalScale(90)
     },
     descInputStyle: {
         height: moderateVerticalScale(80),
@@ -215,7 +333,7 @@ const styles = StyleSheet.create({
         borderRadius: moderateScale(11),
         marginTop: moderateVerticalScale(1),
         color: '#FFFFFF', // add this line
-        marginBottom:moderateVerticalScale(25)
+        marginBottom: moderateVerticalScale(25)
     },
     unselectedButton: {
         marginTop: moderateVerticalScale(1),
@@ -224,7 +342,7 @@ const styles = StyleSheet.create({
         width: moderateScale(95),
         borderRadius: moderateScale(11),
         marginTop: moderateVerticalScale(1),
-        marginBottom:moderateVerticalScale(25)
+        marginBottom: moderateVerticalScale(25)
     },
     addImageStyle: {
         justifyContent: 'center',
